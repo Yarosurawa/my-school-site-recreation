681e82460a4c0ba8c23d8a53730d9e4a608e68bb
Yarosurawa <yarosurawa@gmail.com>
2022-11-13 19:12:07 +0200

    scroller

---
+++ js/header.js
@@ -0,0 +1,29 @@
+var hbg = document.querySelectorAll(".header-bg-img");
+var delay = 5000;
+function callback() {
+    var _loop_1 = function (i) {
+        setTimeout(function () {
+            hbg[i + 1].classList.remove('right');
+            hbg[i].classList.add('left');
+        }, 5000 * i + delay);
+        setTimeout(function () {
+            hbg[i].style.zIndex = -11;
+            hbg[i].classList.remove('left');
+            hbg[i].classList.add('right');
+        }, 5000 * i + 5000 + delay);
+        setTimeout(function () {
+            hbg[i].style.zIndex = -10;
+        }, 5000 * i + 10000 + delay);
+    };
+    for (var i = 0; i < 4; i++) {
+        _loop_1(i);
+    }
+    setTimeout(function () {
+        hbg[0].classList.remove('right');
+        hbg[0].classList.remove('left');
+        hbg[3].classList.add('left');
+        hbg[3].classList.remove('right');
+        callback();
+    }, 20000);
+}
+callback();
---
+++ ts/header.ts
@@ -0,0 +1,28 @@
+const hbg:Object = document.querySelectorAll(".header-bg-img");
+const delay:number = 5000;
+
+function callback():void {
+    for(let i = 0; i < 4; i++ ) {
+        setTimeout(()=>{
+            hbg[i + 1].classList.remove('right')
+            hbg[i].classList.add('left')
+        }, 5000*i+delay)
+        setTimeout(()=>{
+            hbg[i].style.zIndex = -11;
+            hbg[i].classList.remove('left')
+            hbg[i].classList.add('right')
+        }, 5000*i+5000+delay)
+        setTimeout(()=>{
+            hbg[i].style.zIndex = -10;
+        }, 5000*i+10000+delay)
+    }
+    setTimeout(()=>{
+        hbg[0].classList.remove('right')
+        hbg[0].classList.remove('left')
+        hbg[3].classList.add('left')
+        hbg[3].classList.remove('right')
+        callback();
+    }, 20000)
+}
+
+callback()
