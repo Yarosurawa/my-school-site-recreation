8d1e99f0a3b1ca4c4b299466894d1fee38b622f0
Yarosurawa <yarosurawa@gmail.com>
2022-11-13 20:05:06 +0200

    load scroll fix

--- js/loading.js
+++ js/loading.js
@@ -1,6 +1,8 @@
 var loadel = document.getElementById('load');
 document.querySelector("body").onload = function () {
     loadel.style.opacity = 0;
+    document.querySelector("body").style.overflow = "auto";
+    document.querySelector("body").style.overflowX = "hidden";
     setTimeout(function () {
         loadel.remove();
     }, 500);
--- ts/loading.ts
+++ ts/loading.ts
@@ -2,6 +2,8 @@ const loadel:any = document.getElementById('load')
 
 document.querySelector("body").onload = ()=> {
     loadel.style.opacity = 0;
+    document.querySelector("body").style.overflow = "auto"
+    document.querySelector("body").style.overflowX = "hidden"
     setTimeout(()=>{
         loadel.remove()
     }, 500)
