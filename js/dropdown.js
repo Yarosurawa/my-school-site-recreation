02de33c82a05480acf48e8d4c0988d5c5eb40c17
Yarosurawa <yarosurawa@gmail.com>
2022-11-17 14:40:57 +0200

    dfmgodi

---
+++ js/dropdown.js
@@ -0,0 +1,7 @@
+var nav = document.querySelector('.nav');
+// const navElem:HTMLElement | any = nav.children;
+// navElem.forEach((navEl:HTMLElement) => {
+//     navEl.onmouseover = ()=>{
+//         alert('potato')
+//     }
+// });
