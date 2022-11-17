cd2cc211359a673c4f8c86659db3539d4fae5616
Yarosurawa <yarosurawa@gmail.com>
2022-11-17 14:40:52 +0200

    potatoews

---
+++ ts/dropdown.ts
@@ -0,0 +1,8 @@
+const nav:HTMLElement = document.querySelector('.nav');
+// const navElem:HTMLElement | any = nav.children;
+
+// navElem.forEach((navEl:HTMLElement) => {
+//     navEl.onmouseover = ()=>{
+//         alert('potato')
+//     }
+// });
